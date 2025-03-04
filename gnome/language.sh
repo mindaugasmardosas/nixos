1ace7352e2f8c9179c82c24f4ad771039a4a9671
Mindaugas Mardosas <m.mardosas@ik.me>
2025-03-05 00:59:34 +0200

    Gnome settings script

--- gnome/language.sh
+++ gnome/language.sh
@@ -1,3 +1,16 @@
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'lt')]"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>space']"
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+dconf write /org/gnome/nautilus/preferences/always-use-location-entry true
+
+# Kdrive
+cat <<EOF > ~/.local/share/applications/kDrive.desktop
+[Desktop Entry]
+Name=kDrive
+Comment=Run kDrive
+Exec=appimage-run /home/mm/bin/kDrive
+Icon=/home/mm/Pictures/icons/kDrive.png
+Type=Application
+Categories=Utility;Application;
+EOF
 
--- update.sh
+++ update.sh
@@ -7,18 +7,3 @@ nixos-rebuild switch
 dconf write /org/gnome/nautilus/preferences/always-use-location-entry true
 nix-collect-garbage --delete-older-than 7d
 
-# Kdrive
-cat <<EOF > ~/.local/share/applications/kDrive.desktop
-[Desktop Entry]
-Name=kDrive
-Comment=Run kDrive
-Exec=appimage-run /home/mm/bin/kDrive
-Icon=/home/mm/Pictures/icons/kDrive.png
-Type=Application
-Categories=Utility;Application;
-EOF
-
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>space']"
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'lt')]"
